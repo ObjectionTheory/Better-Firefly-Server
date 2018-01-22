b76f03787205faa5291a4541d183e979eda6d5b4
ObjectionTheory <therandomguy265@gmail.com>
2018-01-22 12:35:35 +0000

    Auth

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,23 @@
 import flask
+from flash_httpauth import HTTPBasicAuth
 
 app = flask.Flask(__name__)
+auth = HTTPBasicAuth()
 
-@application.route('/')
+@app.route('/')
 def get_status():
     return "Hello, I am running..."
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/summary')
+@auth.login_required
+def return_summary():
+    return "Hi!"
 
 if __name__ == '__main__':
     app.run()
